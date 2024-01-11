70104c098abce141016adeff586b77c2485a2586
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2024-01-10 21:26:55 -0600

    Feat: Added functionality for the uploaded image file to appear on the background.

---
+++ script.js
@@ -0,0 +1,11 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage() {
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;  
+}
+
