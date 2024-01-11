a9dec68ef4468038ce1c19a6ee0dc3091fc5dc72
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2024-01-10 21:28:20 -0600

    Feat: Removed text content after image has been uploaded.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ inputFile.addEventListener("change", uploadImage);
 
 function uploadImage() {
     let imgLink = URL.createObjectURL(inputFile.files[0]);
-    imageView.style.backgroundImage = `url(${imgLink})`;  
+    imageView.style.backgroundImage = `url(${imgLink})`; 
+    imageView.textContent ="";
 }
 
